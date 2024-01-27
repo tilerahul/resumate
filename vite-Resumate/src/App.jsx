150613c152f25915f82050821736fd587580ac4a
om2438164 <om2438164@gmail.com>
2024-01-27 23:01:56 +0530

    Project Created

---
+++ vite-Resumate/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Body from '../components/body'
+function App() {
+
+  return (
+    <>
+      <Body/>
+    </>
+  )
+}
+
+export default App
