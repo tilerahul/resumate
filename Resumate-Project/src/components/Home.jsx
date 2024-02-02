013f03ef5df91a9ab5c27529e278360539c1328e
om2438164 <om2438164@gmail.com>
2024-02-02 21:24:15 +0530

    Hero section Added

--- Resumate-Project/src/components/Home.jsx
+++ Resumate-Project/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
-
+import Hero from './Hero';
 function Home() {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   );
 }
--- Resumate-Project/src/components/Navbar.jsx
+++ Resumate-Project/src/components/Navbar.jsx
@@ -72,7 +72,7 @@ function Navbar() {
                     </button>
                 </div>
                 <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-                    <li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a></li>
+                    <li><Link to="/" className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</Link></li>
                     <li className="text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
