25c41bdd7a4d68cb381c30d947c301a25929ea0a
Rahul Tile <tilerahul8068@gmail.com>
2024-02-11 09:39:06 +0530

    working FAQ component

--- Resumate-Project/src/components/faq/FAQComponent.jsx
+++ Resumate-Project/src/components/faq/FAQComponent.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { FaPlus } from "react-icons/fa";
+import React, { useState } from 'react'
+import { FaPlus, FaMinus } from "react-icons/fa";
 
-const faqComponent = ({id, question, answer}) => {
+const faqComponent = ({ id, question, answer }) => {
+  
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <div className='shadow-xl my-4 rounded-md'>
         <div className='flex items-center justify-between'>
           <p className='px-5 pt-3 m-2 font-medium'>Q.{id} {question}</p>
-          <span className='mx-16'><FaPlus /></span>
+          <span className='mx-16'>{show ? <FaMinus onClick={() => setShow(false)}className='cursor-pointer' /> : <FaPlus onClick={() => setShow(true)}className='cursor-pointer' />}</span>
         </div>
-        <div>
-          <p className='px-5 py-3 mx-9 pb-4'>{answer}</p>
-        </div>
+        {show &&
+          <div className='border-l-4 border-sky-800 rounded'>
+            <p className='px-5 py-3 mx-9 pb-4'>{answer}</p>
+          </div>
+        }
       </div>
     </>
   )
