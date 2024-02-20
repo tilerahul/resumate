2ee2dbab96b8b2ea6d57873f5bf6bc3d56aba51c
om2438164 <om2438164@gmail.com>
2024-02-20 10:47:34 +0530

    why choose us section added

--- Resumate-Project/src/components/feature.jsx
+++ Resumate-Project/src/components/feature.jsx
@@ -66,11 +66,77 @@ function Feature() {
             </section>
 
 
+            {/* why choose us section */}
             <section>
-              
+            <div class="bg-gray-200 px-2 py-10">
 
+                <div id="features" class="mx-auto max-w-6xl">
+                    
+                    <h2 class="text-center font-display text-3xl font-bold tracking-tight text-slate-900 md:text-4xl">
+                    Why Use Resumate, resume builder?
+                    </h2>
+                    <br />
+                    <p class="text-center text-base font-semibold leading-7 text-primary-500">The best resume builder for your needs</p>
+                    <ul class="mt-16 grid grid-cols-1 gap-6 text-center text-slate-700 md:grid-cols-3">
+                    <li class="rounded-xl bg-white px-6 py-8 shadow-sm">
 
-              
+                        <img src="free.svg" alt="" class="mx-auto h-10 w-10"/>
+                        <h3 class="my-3 font-display font-medium">Totally Free</h3>
+                        <p class="mt-1.5 text-sm leading-6 text-secondary-500">
+                        Enjoy our free feature. If you want to give your resume an extra boost, feel free to use our resume builder at no cost.
+                        </p>
+
+                    </li>
+                    <li class="rounded-xl bg-white px-6 py-8 shadow-sm">
+
+                        <img src="professional.svg"
+                                alt="" class="mx-auto h-10 w-10"/>
+                        <h3 class="my-3 font-display font-medium">Professional Resume Templates</h3>
+                        <p class="mt-1.5 text-sm leading-6 text-secondary-500">
+                        Find the perfect template here! Whether you prefer a classic black & white design or something more creative we've got right on for you.
+                        </p>
+
+                    </li>
+                    <li class="rounded-xl bg-white px-6 py-8 shadow-sm">
+                        <img src="free.svg" alt="" class="mx-auto h-10 w-10"/>
+                        <h3 class="my-3 font-display font-medium">No Hidden Fees</h3>
+                        <p class="mt-1.5 text-sm leading-6 text-secondary-500">
+                        We value transparency. With us, what you see is what you get. There are no hidden fees or charges.
+                        </p>
+
+                    </li>
+                    <li class="rounded-xl bg-white px-6 py-8 shadow-sm">
+                        <a href="/pricing" class="group">
+                        <img src="AtsFriendly.svg" alt="" class="mx-auto h-10 w-10"/>
+                        <h3 class="my-3 font-display font-medium group-hover:text-primary-500">ATS Friendly</h3>
+                        <p class="mt-1.5 text-sm leading-6 text-secondary-500">Our templates are designed to be Applicant Tracking System (ATS) friendly. This means they are optimized to make it through automated resume screening software used by many employers.</p>
+                        </a>
+                    </li>
+                    <li class="rounded-xl bg-white px-6 py-8 shadow-sm">
+                        <a href="/templates" class="group">
+                        <img src="editResume.svg" alt="" class="mx-auto h-10 w-10"/>
+                        <h3 class="my-3 font-display font-medium group-hover:text-primary-500">
+                        Edit Your Resume in Real Time
+                        </h3>
+                        <p class="mt-1.5 text-sm leading-6 text-secondary-500">Our resume builder allows you to edit and update your resume in real time. This means you can see changes as you make them, allowing for easy customization and refinement. </p>
+                        </a>
+                    </li>
+                    <li class="rounded-xl bg-white px-6 py-8 shadow-sm">
+                        <a href="/download" class="group">
+                        <img src="EasyToUse.svg" alt="" class="mx-auto h-10 w-10"/>
+                        <h3 class="my-3 font-display font-medium group-hover:text-primary-500">Easy to Use</h3>
+                        <p class="mt-1.5 text-sm leading-6 text-secondary-500">Our resume builder is user-friendly and intuitive. Even if you’re not tech-savvy, you’ll find our platform easy to navigate and use.</p>
+                        </a>
+                    </li>
+                    </ul>
+                </div>
+
+            </div>  
+            </section>
+
+            {/* Review Section */}
+            <section>
+
             </section>
 
 
