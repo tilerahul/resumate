881a19793e8d96156c44683117f5cc712538ed18
om2438164 <om2438164@gmail.com>
2024-03-09 16:57:17 +0530

    updated

--- Resumate-Project/src/components/Resume/Section/Skills.jsx
+++ Resumate-Project/src/components/Resume/Section/Skills.jsx
@@ -45,7 +45,7 @@ const Skills = () => {
       toast.success("Skill saved successfully");
       clearForm();
     }
-    setSection('workExperience');
+    
   };
 
   const addFields = () => {
@@ -127,7 +127,7 @@ const Skills = () => {
             onClick={() => clearForm()}
             className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
-            Cancel
+            Next
           </button>
         </div>
       </form>
